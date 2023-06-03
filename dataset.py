cd83539d855dcacb3de80d23bbd82fb462580fd7
kyutech-tatsuma <131667347+kyutech-tatsuma@users.noreply.github.com>
2023-06-03 12:34:31 +0900

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -3,15 +3,14 @@ import torch
 import torch.nn as nn
 import torchvision.transforms as transforms
 from torch.utils.data import Dataset, DataLoader
-
 import cv2
 from pydub import AudioSegment
-
+import math
 import numpy as np
 
 # データセットクラスの定義
 class VideoAudioDataset(Dataset):
-    def __init__(self, video_files, targets, transform=None, resize_shape=(112, 112)):
+    def __init__(self, video_files, targets, transform=None, resize_shape=(112, 112)):# 30 seconds at 44.1 kHz
         self.video_files = video_files
         self.targets = targets
         self.transform = transform
@@ -22,10 +21,9 @@ class VideoAudioDataset(Dataset):
 
     def __getitem__(self, idx):
         max_frames = 5000
+        max_audio_length = 44100 * 30  # 30 seconds at 44.1 kHz
 
         target= self.targets[idx]
-        # Max length of audio samples
-        max_audio_length = 44100 * 30  # 30 seconds at 44.1 kHz
 
         video_file = self.video_files[idx]
 
@@ -42,14 +40,14 @@ class VideoAudioDataset(Dataset):
             frames.append(frame)
         video.release()
 
-                # Downsample frames if necessary
+        # Downsample frames if necessary
         if len(frames) > max_frames:
             frames = frames[::len(frames)//max_frames][:max_frames]
         frames = np.array(frames)
         frames = np.transpose(frames, (3, 0, 1, 2))
         # Load audio and convert to spectrogram
         audio = AudioSegment.from_file(video_file)
-        video = cv2.VideoCapture(video_file)
+
         audio_samples = audio.get_array_of_samples()
         # Pad or trim audio samples
         if len(audio_samples) > max_audio_length:
@@ -57,7 +55,9 @@ class VideoAudioDataset(Dataset):
         else:
             audio_samples += [0] * (max_audio_length - len(audio_samples))
         audio_spectrogram = np.abs(np.fft.rfft(audio_samples))
+        
+        print("Frames shape:", frames.shape)
+        print("Audio spectrogram shape:", audio_spectrogram.shape)
 
-        # print("Frames shapes: ", [frame.shape for frame in frames])
 
         return torch.tensor(frames), torch.tensor(audio_spectrogram), target
